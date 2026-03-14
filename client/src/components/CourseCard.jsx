5b02fa4baf87c16e25b9379fc9d6e620fccebbc1
garethS-debug <76433148+garethS-debug@users.noreply.github.com>
2026-03-14 10:59:35 +0000

    Setting Up character picker cards

--- client/src/components/CourseCard.jsx
+++ client/src/components/CourseCard.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 
 import { Link } from 'react-router-dom'
+import CharacterPicker from './CharacterPicker'
 
 const CourseCard = ({course}) => {
   return (
     <div className="card">
         <div className="card-title">Course Number: {course.id}</div>
         <div className="card-description">Course Title: {course.title}</div>
+        <CharacterPicker />
         <div className="card-options">
           <Link className="button" to={`/course/${course.id}`}>View Details</Link>
           <Link className="button" to={`/course/${course.id}`}>Enroll</Link>
