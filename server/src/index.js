b5a955004f3229321853ef6938d0b853d6d99a5e
GarethSwarte <garethjswarte@gmail.com>
2026-03-23 20:07:51 +0000

    changing host url

--- server/src/index.js
+++ server/src/index.js
@@ -4,7 +4,7 @@ const app = require("./app");
 const { connectDatabase } = require("./models");
 
 const PORT = process.env.PORT || 5000;
-const HOST = process.env.HOST || "127.0.0.1";
+const HOST = process.env.HOST || "0.0.0.0";
 
 async function startServer() {
   try {
